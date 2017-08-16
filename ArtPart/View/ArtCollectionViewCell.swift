aeb114bba3bb45df555f4fa786f486c6f5bfa9bf
Mihir Thanekar <mihirthanekar@gmail.com>
2017-08-16 14:45:02 -0700

    Added custom collection view cell.

---
+++ ArtPart/View/ArtCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArtCollectionViewCell.swift
+//  ArtPart
+//
+//  Created by Mihir Thanekar on 8/16/17.
+//  Copyright © 2017 Mihir Thanekar. All rights reserved.
+//
+
+import UIKit
+
+class ArtCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var artImage: UIImageView!
+    @IBOutlet weak var suggestedPrice: UILabel!
+    
+}
