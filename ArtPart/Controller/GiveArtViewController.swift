effda0b36f9f6e9c6ef4879f5a4cf6956863e5a6
Mihir Thanekar <mihirthanekar@gmail.com>
2017-08-18 15:54:26 -0700

    UITextFieldDelegate in POST view.

--- ArtPart/Controller/GiveArtViewController.swift
+++ ArtPart/Controller/GiveArtViewController.swift
@@ -12,6 +12,8 @@ class GiveArtViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var selectedImages = [UIImage]()
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,8 @@ class GiveArtViewController: UIViewController {
                 selectedImages.append(#imageLiteral(resourceName: "placeholder-image"))
             }
         }
+        nameField.delegate = self
+        descriptionField.delegate = self
     }
 
 
@@ -70,4 +74,15 @@ extension GiveArtViewController: UIImagePickerControllerDelegate, UINavigationCo
         }
     }
     
+}
+
+extension GiveArtViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
